ef7e94bd7215d909d718d55392518cc2a4c95900
Shaun Ashby <shaun.ashby@gmail.com>
2020-09-17 13:41:20 +0200

    Add test project for managing unit tests

---
+++ HellowTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HellowTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
