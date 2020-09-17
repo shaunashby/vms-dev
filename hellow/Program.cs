915bcb75b8ca84a4fbc6a743d2017177e52cb904
Shaun Ashby <Shaun.Ashby@gmail.com>
2020-09-17 12:20:35 +0200

    Add a simple console project

---
+++ hellow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hellow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
